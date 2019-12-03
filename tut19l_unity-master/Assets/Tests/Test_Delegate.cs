0093172c59231301997f1ba4d8a72e1b25687859
RikuInada <ricefieldcontinent@keio.jp>
2019-12-03 13:31:37 +0900

    な、ななんと！チュートリアルが上がっちまいました！

---
+++ tut19l_unity-master/Assets/Tests/Test_Delegate.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Test_Delegate
+    {
+        // delegate の宣言
+        delegate string HogeHogeFunc ();
+
+        // delegate を利用した変数定義
+        HogeHogeFunc onFuncHogeHoge;
+
+        // event を定義
+        event HogeHogeFunc onHogeListner;
+
+        string func00 (){
+            return "func00";
+        }
+
+        string func01 (){
+            return "func01";
+        }
+
+
+
+        [Test]
+        public void Test_Delegate_Func00 ()
+        {
+            onFuncHogeHoge = func00;            //← デリゲート変数に関数を登録
+            string result = onFuncHogeHoge ();  //← デリゲートを実行
+
+            Assert.AreEqual ("func00", result);
+        }
+
+        [Test]
+        public void Test_Delegate_Func01 ()
+        {
+            string result;
+
+            onFuncHogeHoge = func01;
+            result = onFuncHogeHoge ();             //<- デリゲート実行
+            
+            Assert.AreEqual ("func01", result);
+        }
+
+        [Test]
+        public void Test_Delegate_Func02 ()
+        {
+            string result;
+
+            onFuncHogeHoge = () => {
+                Debug.Log ("func02");
+                return "func02";
+            };
+            result = onFuncHogeHoge ();             //<- デリゲート実行
+
+            Assert.AreEqual ("func02", result);
+        }
+    }
+}
